41c071bf214317019c4798777305fb33f9db3895
int <int_eth@proton.me>
2023-04-13 14:04:56 -0500

    feat(programs): counter program extend
    Add a function that initializes a counter account
    to track count state.

--- programs/anchor-test/src/lib.rs
+++ programs/anchor-test/src/lib.rs
@@ -10,7 +10,32 @@ pub mod anchor_test {
         msg!("hi");
         Ok(())
     }
+
+    pub fn initialize_counter(ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        msg!("counter init");
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct SayHello {}
+
+#[account]
+pub struct Counter {
+    count: u64,
+}
+
+// 3 public keys
+// counter - account that will store count
+// signer - payer
+// system_program - will own counter account
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8 + 8)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
