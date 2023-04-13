c7e9a4e58d3a632ad3d49ac4849d8fc6153ebeb1
int <int_eth@proton.me>
2023-04-13 12:44:44 -0500

    feat(program): create program
    Program with simple function.
    Logs message to Sol transaction logs.

--- programs/anchor-test/src/lib.rs
+++ programs/anchor-test/src/lib.rs
@@ -1,15 +1,16 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("5nW66C8SHfsfUBbsAm9bRcbpn9tLHESjFSaJUTyGrjsF");
 
 #[program]
 pub mod anchor_test {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn say_hello(_ctx: Context<SayHello>) -> Result<()> {
+        msg!("hi");
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SayHello {}
