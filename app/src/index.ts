65d489a87b26f5774d488f30ac19c473f7468842
int <int_eth@proton.me>
2023-04-13 14:48:13 -0500

    feat(app) call counter init from client

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,6 +1,7 @@
-import { Connection } from '@solana/web3.js';
+import { Connection, Keypair } from '@solana/web3.js';
 import * as dotenv from 'dotenv';
 
+import { PROGRAM_ID } from 'config';
 import { getProgram, getProvider, loadWallet } from 'utils';
 
 dotenv.config();
@@ -10,10 +11,21 @@ const main = async () => {
   const wallet = loadWallet();
 
   const provider = getProvider(connection, wallet);
-  const program = getProgram(provider);
+  const program = getProgram(provider, PROGRAM_ID);
 
   const tx = await program.methods.sayHello().rpc();
   console.log(tx);
+
+  // new keypair for counter acc
+  const counterAccount = Keypair.generate();
+
+  // initialize counter
+  const sig = await program.methods
+    .initializeCounter()
+    .accounts({ counter: counterAccount.publicKey })
+    .signers([counterAccount])
+    .rpc();
+  console.log(sig);
 };
 
 main();
