842b037d7a4368f8e9187c76fca182e36019745c
int <int_eth@proton.me>
2023-04-13 14:47:00 -0500

    fix(app) getProgram bug
    Returning undefined previously.
    Fix to ensure it returns the program based on the IDL.

--- app/src/utils/program.ts
+++ app/src/utils/program.ts
@@ -1,8 +1,5 @@
 import * as anchor from '@coral-xyz/anchor';
-import { AnchorTest } from '../../../target/types/anchor_test';
+import { IDL } from '../../../target/types/anchor_test';
 
-export const getProgram = (provider: anchor.Provider) => {
-  anchor.setProvider(provider);
-  const program = anchor.workspace.AnchorTest as anchor.Program<AnchorTest>;
-  return program;
-};
+export const getProgram = (provider: anchor.Provider, programId: string) =>
+  new anchor.Program(IDL, programId, provider);
